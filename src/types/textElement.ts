b12f74a20b9cc057b6ef2b8d776953301bd02880
OS X <rishabhdhawan@icloud.com>
2024-10-03 01:08:18 +0530

    feat: add TextElement type to narrow down text element types

---
+++ src/types/textElement.ts
@@ -0,0 +1,8 @@
+/**
+ * Type created to narrow down the type of elements that can be used as text elements.
+ */
+export type TextElement =
+  | HTMLParagraphElement
+  | HTMLHeadingElement
+  | HTMLSpanElement
+  | HTMLDivElement;
