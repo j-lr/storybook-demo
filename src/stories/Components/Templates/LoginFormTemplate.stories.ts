2b2f7ce279ccca6784f1d4e7354d97468c317577
OS X <rishabhdhawan@icloud.com>
2024-10-03 02:50:34 +0530

    feat(LoginFormTemplate.tsx) : add optional props
    add props 'title' and 'errorMessage'

--- src/stories/Components/Templates/LoginFormTemplate.stories.ts
+++ src/stories/Components/Templates/LoginFormTemplate.stories.ts
@@ -1,48 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import LoginFormTemplate from "./LoginFormTemplate";
+import { fn } from "@storybook/test";
 
-import type { Meta, StoryObj } from '@storybook/react';
-import LoginFormTemplate from './LoginFormTemplate';
-import { fn } from '@storybook/test';
+const meta = {
+  title: "Components/Templates/LoginFormTemplate",
+  component: LoginFormTemplate,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 
-
-
-
-
-const meta ={
-    title: 'Components/Templates/LoginFormTemplate',  
-    component:LoginFormTemplate,
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
-
-  args: { onLoginClick: fn(),
-    onSignUpClick: fn() },
-    
+  args: { onLoginClick: fn(), onSignUpClick: fn() },
 } satisfies Meta<typeof LoginFormTemplate>;
 
 export default meta;
 
 type Story = StoryObj<typeof LoginFormTemplate>;
 
-
 export const Default: Story = {
-    name:'Default',
-    args: {
-
-    },
+  name: "Default",
+  args: {
+    title: "Web-gym",
+  },
 };
 export const Error: Story = {
-    name:'Error',
-    args: {
-
-    },
+  name: "Error",
+  args: {
+    title: "Web-gym",
+    errorMessage: "Invalid credentials",
+  },
 };
 
-
 export const WithAutoFocus: Story = {
-    name: 'With Auto Focus',
-    args: {
-
-
-    },
+  name: "With Auto Focus",
+  args: {},
 };
--- src/stories/Components/Templates/LoginFormTemplate.tsx
+++ src/stories/Components/Templates/LoginFormTemplate.tsx
@@ -1,72 +1,108 @@
 import { Box, Button, Typography } from "@mui/material";
 import AuthFormInput from "../Atoms/AuthFormInput";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 /** Login form Template  */
 export interface LoginFormTemplateProps {
-    onLoginClick: (email: string, password: string) => void;
-    onSignUpClick: () => void;
+  title?: string;
+  errorMessage?: string; // Error message is not displayed if user changes any input or click on Login button
+
+  onLoginClick: (email: string, password: string) => void;
+  onSignUpClick: () => void;
 }
 
 const LoginFormTemplate: React.FC<LoginFormTemplateProps> = ({
-    onLoginClick,
-    onSignUpClick,
+  title,
+  errorMessage,
+  onLoginClick,
+  onSignUpClick,
 }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "background.paper",
-                gap: 4,
-                px: 12,
-                py: 5,
-                minWidth: "320px",
-                minHeight: "400px",
-                borderRadius: 5,
-            }}
-        >
-            <Typography variant="h6" color="text.primary" sx={{ mb: 1 }}>
-                Web-gym
-            </Typography>
-            <AuthFormInput
-                type="email"
-                defaultLabel="e-mail"
-                onChangeListener={(value) => setEmail(value)}
-                errorMessage=""
-                autoFocus={true}
-            />
-            <AuthFormInput
-                type="password"
-                defaultLabel="password"
-                onChangeListener={(value) => setPassword(value)}
-                errorMessage=""
-                autoFocus={false}
-            />
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [renderErrorMessage, setRenderErrorMessage] = useState(!!errorMessage);
 
-            <Button
-                fullWidth
-                variant="contained"
-                color="primary"
-                size="large"
-                onClick={() => onLoginClick(email, password)}
-                sx={{ mt: 2 }}
-            >
-                Login
-            </Button>
+  useEffect(() => {
+    setRenderErrorMessage(false);
+  }, [email, password, errorMessage]);
+  useEffect(() => {
+    setRenderErrorMessage(true);
+  }, [errorMessage]);
 
-            <Typography variant="caption" color="textSecondary" sx={{ mt: 2 }}>
-                New to Web-gym?{" "}
-                <u style={{ cursor: "pointer" }} onClick={onSignUpClick}>
-                    Sign up
-                </u>
-            </Typography>
-        </Box>
-    );
+  const handleLoginClick = () => {
+    setRenderErrorMessage(false);
+    onLoginClick(email, password);
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "background.paper",
+        gap: 4,
+        px: 12,
+        py: 5,
+        minWidth: "320px",
+        minHeight: "400px",
+        borderRadius: 5,
+      }}
+    >
+      {title && (
+        <Typography variant="h6" color="text.primary" sx={{ mb: 1 }}>
+          {title}
+        </Typography>
+      )}
+      {errorMessage && renderErrorMessage && (
+        <Typography variant="overline" color="error" sx={{ mb: 1 }}>
+          {errorMessage}
+        </Typography>
+      )}
+      <AuthFormInput
+        type="email"
+        defaultLabel="e-mail"
+        onChangeListener={(value) => setEmail(value)}
+        errorMessage=""
+        autoFocus={true}
+      />
+      <AuthFormInput
+        type="password"
+        defaultLabel="password"
+        onChangeListener={(value) => setPassword(value)}
+        errorMessage=""
+        autoFocus={false}
+      />
+
+      <Button
+        fullWidth
+        variant="contained"
+        color="primary"
+        size="large"
+        onClick={handleLoginClick}
+        sx={{ mt: 2 }}
+        disabled={email.length === 0 || password.length === 0}
+      >
+        Login
+      </Button>
+
+      {title && (
+        <Typography variant="caption" color="textSecondary" sx={{ mt: 2 }}>
+          New to {title}?{" "}
+          <u style={{ cursor: "pointer" }} onClick={onSignUpClick}>
+            Sign up
+          </u>
+        </Typography>
+      )}
+      {!title && (
+        <Typography variant="caption" color="textSecondary" sx={{ mt: 2 }}>
+          Create an account!{" "}
+          <u style={{ cursor: "pointer" }} onClick={onSignUpClick}>
+            Sign up
+          </u>
+        </Typography>
+      )}
+    </Box>
+  );
 };
 
 export default LoginFormTemplate;
